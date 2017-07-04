fd6fa7d6823b49b1fd519ca2d423cf8a9aaba454
ruan3 <1633486734@qq.com>
2017-07-04 09:56:07 +0800

    创建mvp模式的代码块

---
+++ app/src/main/java/io/agora/presenter/IPresenterImpl.java
@@ -0,0 +1,11 @@
+package io.agora.presenter;
+
+import io.agora.contract.IContract;
+
+/**
+* Created by MVPHelper on 2017/07/04
+*/
+
+public class IPresenterImpl implements IContract.Presenter{
+
+}
