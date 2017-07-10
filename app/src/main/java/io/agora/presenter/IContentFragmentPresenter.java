46ce02d7ad8fd7274edb7186236842a4c9f588fe
ruan3 <1633486734@qq.com>
2017-07-10 16:58:18 +0800

    完成列表信息获取

---
+++ app/src/main/java/io/agora/presenter/IContentFragmentPresenter.java
@@ -0,0 +1,14 @@
+package io.agora.presenter;
+
+/**
+ * File Name:
+ * Author:      ruan
+ * Write Dates: 2017/7/7
+ * Description:
+ */
+
+public interface IContentFragmentPresenter {
+
+    public void getData();
+    public void setData();
+}
