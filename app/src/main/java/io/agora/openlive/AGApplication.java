b62ec15d46a9da5ba11d0f3221feeb7f2087bc64
ruan3 <1633486734@qq.com>
2017-07-10 16:57:47 +0800

    完成列表信息获取

--- app/src/main/java/io/agora/openlive/AGApplication.java
+++ app/src/main/java/io/agora/openlive/AGApplication.java
@@ -1,11 +1,25 @@
 package io.agora.openlive;
 
 import android.app.Application;
+
+import cn.bmob.v3.Bmob;
 import io.agora.openlive.model.WorkerThread;
 
 public class AGApplication extends Application {
 
     private WorkerThread mWorkerThread;
+
+    /**
+     * SDK初始化也可以放到Application中
+     */
+    public static String APPID = "e0e5821ab961e7607a39f5926cae7a51";
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        //第二：默认初始化
+        Bmob.initialize(getApplicationContext(), APPID, "demo");
+    }
 
     public synchronized void initWorkerThread() {
         if (mWorkerThread == null) {
