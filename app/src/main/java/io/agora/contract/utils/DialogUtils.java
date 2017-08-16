0f8aee5fa40b3212f69a64879db3a81d42013e80
ruan3 <1633486734@qq.com>
2017-08-16 17:55:50 +0800

    新增完成客服中心，修改忘记密码逻辑，修改直播页面增加点赞数，礼物数

--- app/src/main/java/io/agora/contract/utils/DialogUtils.java
+++ app/src/main/java/io/agora/contract/utils/DialogUtils.java
@@ -1,13 +1,18 @@
 package io.agora.contract.utils;
 
 import android.content.Context;
+import android.text.TextUtils;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.PopupWindow;
 
+import cn.bmob.v3.BmobUser;
+import cn.bmob.v3.exception.BmobException;
+import cn.bmob.v3.listener.UpdateListener;
 import io.agora.contract.view.BaseDialog;
+import io.agora.model.MyUser;
 import io.agora.openlive.R;
 
 
@@ -66,6 +71,44 @@ public class DialogUtils {
 		dialog.setCancelable(false);
 		dialog.show();
 		
+	}
+
+	public void showBindEmail(final Context context, final CallBackListener reListener){
+
+		this.reListener = reListener;
+		dialog = new BaseDialog(context,R.style.dialog);
+		dialog.setContentView(R.layout.xjj_dialog_bind_email);
+		final EditText et_email = (EditText) dialog.findViewById(R.id.et_email);
+		Button btn_bind_email = (Button) dialog.findViewById(R.id.btn_bind_email);
+
+
+
+		btn_bind_email.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+
+				final String email = et_email.getText().toString();
+				BmobUser.resetPasswordByEmail(email, new UpdateListener() {
+
+					@Override
+					public void done(BmobException e) {
+						if(e==null){
+							reListener.confirm(email);
+						}else{
+							reListener.confirm("失败:" + e.getMessage());
+						}
+					}
+				});
+                dialog.dismiss();
+
+			}
+		});
+
+		dialog.setCancelable(false);
+		dialog.show();
+
 	}
 
 
---
+++ app/src/main/java/io/agora/contract/utils/NewsJsonUtils.java
@@ -0,0 +1,69 @@
+package io.agora.contract.utils;
+
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import io.agora.model.NewsDetialEntity;
+import io.agora.model.NewsEntity;
+
+/**
+ * Created by llf on 2017/4/13.
+ */
+
+public class NewsJsonUtils {
+    private final static String TAG = "NewsJsonUtils";
+
+    /**
+     * 将获取到的json转换为新闻列表对象
+     *
+     * @param res
+     * @param value
+     * @return
+     */
+    public static List<NewsEntity> readJsonDataBeans(String res, String value) {
+        List<NewsEntity> beans = new ArrayList<>();
+        try {
+            JsonParser parser = new JsonParser();
+            JsonObject jsonObj = parser.parse(res).getAsJsonObject();
+            JsonElement jsonElement = jsonObj.get(value);
+            if (jsonElement == null) {
+                return null;
+            }
+            JsonArray jsonArray = jsonElement.getAsJsonArray();
+            for (int i = 1; i < jsonArray.size(); i++) {
+                JsonObject jo = jsonArray.get(i).getAsJsonObject();
+                if (jo.has("skipType") && "special".equals(jo.get("skipType").getAsString())) {
+                    continue;
+                }
+                if (jo.has("TAGS") && !jo.has("TAG")) {
+                    continue;
+                }
+
+                NewsEntity news = JsonUtils.deserialize(jo, NewsEntity.class);
+                beans.add(news);
+            }
+        } catch (Exception e) {
+        }
+        return beans;
+    }
+
+    public static NewsDetialEntity readJsonNewsDetailBeans(String res, String docId) {
+        NewsDetialEntity newsDetailBean = null;
+        try {
+            JsonParser parser = new JsonParser();
+            JsonObject jsonObj = parser.parse(res).getAsJsonObject();
+            JsonElement jsonElement = jsonObj.get(docId);
+            if (jsonElement == null) {
+                return null;
+            }
+            newsDetailBean = JsonUtils.deserialize(jsonElement.getAsJsonObject(), NewsDetialEntity.class);
+        } catch (Exception e) {
+        }
+        return newsDetailBean;
+    }
+}
