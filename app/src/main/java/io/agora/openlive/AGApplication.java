5b795307240754fa1e5bff37935d6f6799c49f61
ruan3 <1633486734@qq.com>
2017-08-16 18:00:34 +0800

    新增完成客服中心，修改忘记密码逻辑，修改直播页面增加点赞数，礼物数

--- app/src/main/java/io/agora/openlive/AGApplication.java
+++ app/src/main/java/io/agora/openlive/AGApplication.java
@@ -1,10 +1,15 @@
 package io.agora.openlive;
 
 import android.app.Application;
+import android.content.Context;
 
 import com.facebook.drawee.backends.pipeline.Fresco;
 import com.facebook.imagepipeline.core.ImagePipelineConfig;
+import com.mob.MobApplication;
+import com.mob.MobSDK;
 
+import cn.bingoogolapple.swipebacklayout.BGASwipeBackHelper;
+import cn.bingoogolapple.swipebacklayout.BGASwipeBackManager;
 import cn.bmob.v3.Bmob;
 import io.agora.openlive.model.WorkerThread;
 
@@ -16,6 +21,9 @@ public class AGApplication extends Application {
      * SDK初始化也可以放到Application中
      */
     public static String APPID = "e0e5821ab961e7607a39f5926cae7a51";
+    Context context;
+
+
 
     @Override
     public void onCreate() {
@@ -24,7 +32,15 @@ public class AGApplication extends Application {
         Bmob.initialize(getApplicationContext(), APPID, "demo");
         ImagePipelineConfig config = ImagePipelineConfig.newBuilder(this).setDownsampleEnabled(true).build();
         Fresco.initialize(this, config);
+        // 必须在 Application 的 onCreate 方法中执行 BGASwipeBackManager.getInstance().init(this) 来初始化滑动返回
+        BGASwipeBackManager.getInstance().init(this);
+        context = this;
+        //初始化SDK
+        if(!(context instanceof MobApplication)){
+            MobSDK.init(context.getApplicationContext());
+        }
     }
+
 
     public synchronized void initWorkerThread() {
         if (mWorkerThread == null) {
@@ -48,4 +64,6 @@ public class AGApplication extends Application {
         }
         mWorkerThread = null;
     }
+
+
 }
