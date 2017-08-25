44ae7d0e6d931d1e369982a16f9885624bda2f91
ruan3 <1633486734@qq.com>
2017-08-25 17:10:27 +0800

    完成了评论界面的开发，完成了应用的更新，完成了混淆和签名apk

--- app/src/main/java/io/agora/contract/utils/Constants.java
+++ app/src/main/java/io/agora/contract/utils/Constants.java
@@ -24,4 +24,14 @@ public class Constants {
     public static final String ALL_RES_URL = "http://s.budejie.com/topic/list/jingxuan/1/budejie-android-6.2.8/0-20.json?market=baidu&udid=863425026599592&appname=baisibudejie&os=4.2.2&client=android&visiting=&mac=98%3A6c%3Af5%3A4b%3A72%3A6d&ver=6.2.8";
 //  
 
+    public static final String TYPE_COMMENT = "type_comment";
+    public static final String TYPE_URL = "type_url";
+    public static final String COMMENT_TITLE = "comment_title";
+    public static final String COMMENT_ORGIN = "comment_orgin";
+    public static final String TEXT_CONTENT = "text_content";
+    public static final String DURATION_VIDEO = "duration_video";
+    public static final String PLAY_TIMES = "play_times";
+    public static final String VIDEO_THUMBS = "video_thumbs";
+    public static final String CONTENT_ID = "cotent_id";
+
 }
--- app/src/main/java/io/agora/contract/utils/DialogUtils.java
+++ app/src/main/java/io/agora/contract/utils/DialogUtils.java
@@ -8,8 +8,10 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.PopupWindow;
 
+import cn.bmob.v3.BmobSMS;
 import cn.bmob.v3.BmobUser;
 import cn.bmob.v3.exception.BmobException;
+import cn.bmob.v3.listener.QueryListener;
 import cn.bmob.v3.listener.UpdateListener;
 import io.agora.contract.view.BaseDialog;
 import io.agora.model.MyUser;
@@ -90,23 +92,46 @@ public class DialogUtils {
 				// TODO Auto-generated method stub
 
 				final String email = et_email.getText().toString();
-				BmobUser.resetPasswordByEmail(email, new UpdateListener() {
 
-					@Override
-					public void done(BmobException e) {
-						if(e==null){
-							reListener.confirm(email);
-						}else{
-							reListener.confirm("失败:" + e.getMessage());
+				if(Validator.isEmail(email)){
+					LogUtils.e("使用邮箱找回密码");
+					//使用邮箱找回密码
+					BmobUser.resetPasswordByEmail(email, new UpdateListener() {
+
+						@Override
+						public void done(BmobException e) {
+							if(e==null){
+								reListener.confirm(email);
+								dialog.dismiss();
+							}else{
+								reListener.confirm("失败:" + e.getMessage());
+								dialog.dismiss();
+							}
 						}
-					}
-				});
-                dialog.dismiss();
+					});
+				}else if(Validator.isMobile(email)){
+					//使用手机验证找回密码
+					BmobSMS.requestSMSCode(email,"小鸡鸡密码重置", new QueryListener<Integer>() {
 
+						@Override
+						public void done(Integer smsId,BmobException ex) {
+							if(ex==null){//验证码发送成功
+								LogUtils.i("短信id："+smsId);//用于查询本次短信发送详情
+								//跳转到重置密码界面
+								dialog.dismiss();
+								ShowResetPwd(context,reListener,smsId);
+							}else{
+								LogUtils.e("手机密码重置失败--->"+ ex.toString());
+							}
+						}
+					});
+				}else{
+					reListener.confirm("失败：输入并不是邮箱或者手机号！");
+					LogUtils.e("输入的不是邮箱或者手机号");
+				}
 			}
 		});
 
-		dialog.setCancelable(false);
 		dialog.show();
 
 	}
@@ -118,6 +143,103 @@ public class DialogUtils {
 				dialog.dismiss();
 			}
 		}
+	}
+
+	/**
+	 * 忘记密码重置（目前那个手机号重置居然没有收到信息，我也是醉了）
+	 * @param context
+	 * @param reListener
+	 * @param smsId
+	 */
+	public void ShowResetPwd(Context context,final CallBackListener reListener,Integer smsId){
+
+		this.reListener = reListener;
+		dialog = new BaseDialog(context,R.style.dialog);
+		dialog.setContentView(R.layout.xjj_dialog_reset_pwd);
+		EditText et_code = (EditText) dialog.findViewById(R.id.et_code);
+		EditText et_new_pwd = (EditText) dialog.findViewById(R.id.et_new_pwd);
+		Button btn_reset = (Button) dialog.findViewById(R.id.btn_reset);
+		if(smsId!=null){
+			et_code.setText(smsId);
+		}
+		final String code = et_code.getText().toString();
+		final String newPwd = et_new_pwd.getText().toString();
+
+		btn_reset.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+
+				if(!TextUtils.isEmpty(code)&&!TextUtils.isEmpty(newPwd)){
+
+					BmobUser.resetPasswordBySMSCode(code,newPwd, new UpdateListener() {
+
+						@Override
+						public void done(BmobException ex) {
+							if(ex==null){
+								reListener.confirm("密码重置成功！");
+								LogUtils.e("手机号密码重置成功！");
+							}else{
+								reListener.confirm("重置失败：code ="+ex.getErrorCode()+",msg = "+ex.getLocalizedMessage());
+								LogUtils.e("手机号密码重置失败--->"+ex.getErrorCode()+",msg = "+ex.getLocalizedMessage());
+							}
+						}
+					});
+				}
+
+			}
+		});
+		dialog.setCancelable(false);
+		dialog.show();
+
+
+	}
+
+	/**
+	 * 修改密码
+	 * @param context
+	 * @param reListener
+	 */
+	public void showChangePwd(Context context,final CallBackListener reListener){
+
+		this.reListener = reListener;
+		dialog = new BaseDialog(context,R.style.dialog);
+		dialog.setContentView(R.layout.xjj_dialog_change_pwd);
+		final EditText et_old_pwd = (EditText) dialog.findViewById(R.id.et_old_pwd);
+		final EditText et_new_pwd = (EditText) dialog.findViewById(R.id.et_new_pwd);
+		Button btn_change = (Button) dialog.findViewById(R.id.btn_change);
+		btn_change.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				 String oldPwd = et_old_pwd.getText().toString();
+				 String newPwd = et_new_pwd.getText().toString();
+				if(!TextUtils.isEmpty(oldPwd)&&!TextUtils.isEmpty(newPwd)){
+					//当旧密码和新密码都不为空时
+					BmobUser.updateCurrentUserPassword(oldPwd, newPwd, new UpdateListener() {
+
+						@Override
+						public void done(BmobException e) {
+							if(e==null){
+								LogUtils.e("修改成功！下次请使用新密码登录");
+								reListener.confirm("修改成功！下次请使用新密码登录");
+								dialog.dismiss();
+							}else{
+								LogUtils.e("修改成功！下次请使用新密码登录");
+								reListener.cancle("修改失败--->"+e.toString());
+								dialog.dismiss();
+							}
+						}
+
+					});
+
+				}else{
+					reListener.cancle("旧密码或新密码为空！");
+				}
+
+			}
+		});
+		dialog.show();
+
+
 	}
 	
 	public interface CallBackListener {
