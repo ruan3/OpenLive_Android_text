6781232159a69c0be672f8d9b8dec1785b31d82f
ruan3 <1633486734@qq.com>
2017-10-09 15:59:23 +0800

    完成了评论界面的开发，完成了应用的更新，完成了混淆和签名apk

--- app/src/main/java/io/agora/contract/viewpager/FunnyPager.java
+++ app/src/main/java/io/agora/contract/viewpager/FunnyPager.java
@@ -1,6 +1,7 @@
 package io.agora.contract.viewpager;
 
 import android.content.Context;
+import android.graphics.Rect;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.widget.DefaultItemAnimator;
@@ -11,16 +12,23 @@ import android.view.View;
 
 import java.util.List;
 
+import fm.jiecao.jcvideoplayer_lib.JCVideoPlayer;
 import io.agora.contract.adapter.OtherFragmentAdapter;
 import io.agora.contract.fragment.IOtherFragment;
 import io.agora.contract.utils.CacheUtils;
 import io.agora.contract.utils.Constants;
 import io.agora.contract.utils.LogUtils;
 import io.agora.contract.view.EndLessOnScrollListener;
+import io.agora.contract.view.MyJCVideoPlayerStandard;
 import io.agora.model.NetAudioPagerData;
 import io.agora.openlive.R;
 import io.agora.presenter.IOtherFragmentPresenter;
 import io.agora.presenter.IOtherFragmentPresenterImpl;
+
+import static android.support.v7.widget.RecyclerView.SCROLL_STATE_DRAGGING;
+import static android.support.v7.widget.RecyclerView.SCROLL_STATE_SETTLING;
+import static android.widget.AbsListView.OnScrollListener.SCROLL_STATE_FLING;
+import static android.widget.AbsListView.OnScrollListener.SCROLL_STATE_IDLE;
 
 /**
  * File Name:
@@ -45,6 +53,12 @@ public class FunnyPager extends BasePager implements IOtherFragment,SwipeRefresh
     Context context;
 
     List<NetAudioPagerData.ListEntity> mDatas;
+
+    int VisiableCount;//可见条目
+    int firstItem;//第一个可见条目
+    int LastItem;//最后一个可见条目
+
+    MyJCVideoPlayerStandard videoView;//列表中视频对象
 
     public FunnyPager(Context context){
         this.context = context;
@@ -91,6 +105,41 @@ public class FunnyPager extends BasePager implements IOtherFragment,SwipeRefresh
 //        other_recyclerView.setHasFixedSize(true);
         other_recyclerView.setItemAnimator(new DefaultItemAnimator());
 
+
+        other_recyclerView.addOnScrollListener(new RecyclerView.OnScrollListener() {
+            @Override
+            public void onScrollStateChanged(RecyclerView recyclerView, int newState) {
+
+                switch (newState){
+                    case SCROLL_STATE_IDLE://停止滚动
+                        LogUtils.e("recyclerView滚动状态---->停止滚动");
+                        /**在这里执行，视频的自动播放与停止*/
+                        autoPlayVideo(recyclerView);
+                        break;
+                    case SCROLL_STATE_DRAGGING://拖动
+                        LogUtils.e("recyclerView滚动状态---->拖动");
+                        break;
+                    case SCROLL_STATE_SETTLING://惯性滑动
+                        LogUtils.e("recyclerView滚动状态---->惯性滑动");
+                        break;
+                }
+
+
+            }
+
+            @Override
+            public void onScrolled(RecyclerView recyclerView, int dx, int dy) {
+
+                LogUtils.e("recyclerView滚动距离---->x==="+dx+"-----y==="+dy);
+                firstItem = manager.findFirstVisibleItemPosition();
+                LastItem = manager.findLastVisibleItemPosition();
+                VisiableCount = LastItem - firstItem;
+                LogUtils.e("recyclerView可见条目---->第一个==="+firstItem+"-----第二个==="+LastItem+"可显示总数--->"+VisiableCount);
+
+            }
+        });
+
+
         /**
          * 浮点监听
          */
@@ -104,6 +153,41 @@ public class FunnyPager extends BasePager implements IOtherFragment,SwipeRefresh
 
             }
         });
+
+    }
+
+
+    /**
+     * 自动播放停止视频方法
+     */
+    private void autoPlayVideo(RecyclerView recyclerView){
+
+
+        for(int i=0;i<VisiableCount;i++){
+            if(recyclerView != null && recyclerView.getChildAt(i) != null &&recyclerView.getChildAt(i).findViewById(R.id.jcv_videoplayer) != null){
+                videoView = (MyJCVideoPlayerStandard) recyclerView.getChildAt(i).findViewById(R.id.jcv_videoplayer);
+                Rect rect = new Rect();
+                videoView.getLocalVisibleRect(rect);
+                int videoHeight = videoView.getHeight();
+                if(rect.top==0&&rect.bottom==videoHeight){
+
+                    if(videoView.currentState== JCVideoPlayer.CURRENT_STATE_NORMAL||videoView.currentState==JCVideoPlayer.CURRENT_STATE_PAUSE){
+
+                        videoView.startVideo();
+
+                    }
+                    return;
+                }
+
+                videoView.releaseAllVideos();
+
+            }else{
+                if(videoView!=null){
+                    videoView.releaseAllVideos();
+                }
+            }
+
+        }
 
     }
 
@@ -156,6 +240,7 @@ public class FunnyPager extends BasePager implements IOtherFragment,SwipeRefresh
             LogUtils.e("解析看看json数据失败");
 
         }
+
 
     }
 
